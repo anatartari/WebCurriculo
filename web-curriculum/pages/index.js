afb02f1091355e41f96d3c13a280e6537ec05ace
Ana Tartari <anatartari412@gmail.com>
2021-03-25 22:37:48 -0300

    Novo projeto + material ui

---
+++ web-curriculum/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Grid from "@material-ui/core/Grid";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Grid>
+          <span>jksdhgfkh</span>
+        </Grid>
+      </main>
+    </div>
+  );
+}
